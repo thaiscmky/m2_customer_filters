0f2d730bfc2da1e3c03d54b5cc5e82102181b453
thaiscmky <thaiscmky@github.com>
2021-11-24 00:34:53 -0500

    Creates module registration files

---
+++ app/code/ThaisCmky/CustomerFilters/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author      Thais Cailet <thaiscmky@users.noreply.github.com>
+ * @package     ThaisCmky_CustomerFilters
+ * @copyright   Copyright (c) 2021 Thais Cailet (https://thaiscmky.github.io/)
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'ThaisCmky_CustomerFilters', __DIR__);
