f2ec2a64fbeab56aca04c1bd427feafa58192e9f
thaiscmky <thaiscmky@github.com>
2021-11-27 02:14:39 -0500

    Adds frontend route and template page

---
+++ app/code/ThaisCmky/CustomerFilters/Controller/Account/Index.php
@@ -0,0 +1,36 @@
+<?
+
+namespace ThaisCmky\CustomerFilters\Controller\Account;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface, HttpPostActionInterface
+{
+    /**
+     * @var PageFactory
+     */
+    private $pageFactory;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
+     * @param PageFactory $pageFactory
+     * @param RequestInterface $request
+     */
+    public function __construct(PageFactory $pageFactory, RequestInterface $request)
+    {
+        $this->pageFactory = $pageFactory;
+        $this->request = $request;
+    }
+
+    public function execute()
+    {
+        die('hi');
+        return $this->pageFactory->create();
+    }
+}
